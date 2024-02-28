dc6efe65736fa4b13191b89183371a1cba569039
Jakub <jakmil87@gmail.com>
2024-02-28 20:02:25 +0100

    Dodano punkty i przeszkody
    Punkty działają, w generacji przeszkód jest błąd

--- Assets/Scripts/WorldScroller.cs
+++ Assets/Scripts/WorldScroller.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class WorldScroller : MonoBehaviour
 {
     public WorldSegment groundLeft, groundRight;
+
+    public GameObject gleft, gRight;
+
     public float speed = 1;
 
-    private void Update()
+    public WorldSegment[] tiles;
+
+    private void FixedUpdate()
     {
         Vector3 delta_pos = new Vector2( -speed * Time.deltaTime, 0);
         groundLeft.transform.position += delta_pos;
@@ -15,12 +20,19 @@ public class WorldScroller : MonoBehaviour
 
         if(groundRight.transform.position.x <= 0)
         {
+            var newTile = Instantiate(tiles[Random.Range(0,tiles.Length)]);
+
+            newTile.transform.parent = gameObject.transform;
+
             Vector3 tileWidth = new Vector2(groundLeft.Width, 0);
             groundLeft.transform.position = groundRight.transform.position + tileWidth;
 
-            var tmp = groundRight;
+
             groundRight = groundLeft;
-            groundLeft = tmp;
+            groundLeft = newTile;
+
+            //Destroy(gleft);
+            gleft = newTile.gameObject;
         }
     }
 }
